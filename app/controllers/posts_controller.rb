e11844b54e9ae486faa9c8f998ec19bc13410098
Anthony Diké <anthonydike@nyu.edu>
2020-01-08 23:20:37 -0500

    Now user can create a post and be redirected to page showing that post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,21 @@ class PostsController < ApplicationController
   def index
     
   end
+  def show
+    @post = Post.find(params[:id]) # shows post by name its assigned
+  end
   def new
     @addpostheader = 'Create a new post!'
   end
   def create
-    render plain: params[:post].inspect
+    #render plain: params[:post].inspect
+    @post = Post.new(post_params) # directs user to their newly made post
+
+    @post.save
+    redirect_to @post
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body) # limits data needed for a post to title and body
   end
 end
